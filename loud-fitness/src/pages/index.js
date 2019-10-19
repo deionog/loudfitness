296dedca80737edd4b51d4b64f4de9dd990b8d35
Deion O'Garro <deion@Deions-MacBook-Pro.local>
2019-10-19 17:48:45 -0400

    Test styling with gatsby

--- loud-fitness/src/pages/index.js
+++ loud-fitness/src/pages/index.js
@@ -2,9 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 
-export default () => <div style={{color:'purple'}}>
+export default () => <div>
         <Link to="/contact/">Contact</Link>
         <Header headerText="Loud Fitness"/>
         <p>Your Transformation awaits</p>
-
     </div>
